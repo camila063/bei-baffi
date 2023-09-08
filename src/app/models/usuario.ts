5ca82ff976bd469ec04400084f9e384ec5f32250
laNICOLEE <nicksva15@gmail.com>
2023-09-08 11:29:30 -0300

    Creacion de shared y actualizacion

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,7 @@
+export interface Usuario {
+    uid: string | any; //id para identificar de firebase
+    nombre: string; //--> nombre
+    email: string; //-->email
+    rol: string; // -->roles
+    contrasena: string;
+}
